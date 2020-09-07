087c048acbc7e981b8c92ee33b8152ffd66806a2
eric922 <eric92289@gmail.com>
2020-09-06 23:22:25 -0400

    Update MortageCalc.rb

--- MortageCalc.rb
+++ MortageCalc.rb
@@ -1,12 +1,3 @@
-=begin
-Input: number(loan amount), number(APR), number(loan duration)
-Output: number( monthy interst) number(loan duration) number(monthly payment)
-Step:
-1. Define a function that asks for the three pieces of information we need.
-2. Convert the yearly loan to a monthly base.
-3. Convert the APR to a monthly interest rate
-
-=end
 def mortage_calc
   puts "Welcome to the mortage Calcuator"
   puts "Please enter your loan amount"
@@ -16,12 +7,7 @@ def mortage_calc
   puts "Please enter the APR for your loan"
   loan_apr = gets.chomp.to_f / 100
   monthly_interest_rate = loan_apr / 12
-  
-
-  monthly_payment = loan_amount * (monthly_interest_rate /(1 - (1 + monthly_interest_rate) **(-loan_months)))
+  monthly_payment = loan_amount * (monthly_interest_rate /(1 - (1 + monthly_interest_rate) ** (-loan_months)))
   puts "Your monthy payment is $" + "#{monthly_payment.round(2)}"
 end 
-
 mortage_calc
-
-
